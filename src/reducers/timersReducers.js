938dd779fd794e96f83da09f737bbb3bc23b8768
ejguzman3988 <eriberto.guzman@outlook.com>
2020-10-17 02:17:43 -0400

    Added a timers reducers

---
+++ src/reducers/timersReducers.js
@@ -0,0 +1,11 @@
+const initialState = {
+    loading: true,
+    timers
+}
+
+export default (state=initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
